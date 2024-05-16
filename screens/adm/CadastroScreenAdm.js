99ecb43c0eeb8249905b523ec90dca9bd2a49951
Vinicius Dias Vieira <viniciusdiasvieira122@hotmail.com>
2024-05-16 17:08:15 -0300

    att links api

--- screens/adm/CadastroScreenAdm.js
+++ screens/adm/CadastroScreenAdm.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { View, Text, Image, TextInput, TouchableOpacity, Alert } from 'react-native';
+import { View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import Animated, { FadeIn, FadeOut, FadeInUp, FadeInDown } from 'react-native-reanimated';
+import Animated, { FadeInUp, FadeInDown } from 'react-native-reanimated';
 import { useNavigation } from '@react-navigation/native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Picker } from '@react-native-picker/picker';
 
 export default function CadastroAdm() {
     const navigation = useNavigation();
@@ -13,21 +14,17 @@ export default function CadastroAdm() {
     const [cpf, setCpf] = useState('');
     const [usuario, setUsuario] = useState('');
     const [senha, setSenha] = useState('');
-
-
-
-
+    const [role, setRole] = useState('');
 
     const formatarTelefone = (value) => {
         const numTelefone = value.replace(/\D/g, '');
         setTelefone(numTelefone.replace(/^(\d{2})(\d{1})(\d{4})(\d{4})$/, '($1) $2 $3-$4'));
     };
-    
 
     const handleSignupAdm = async () => {
         try {
             const token = await AsyncStorage.getItem('token');
-            const response = await fetch('http://192.168.3.15:8080/auth/registro/adm', {
+            const response = await fetch('http://18.206.68.106:8080/auth/registro/all', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
@@ -40,39 +37,37 @@ export default function CadastroAdm() {
                         nome: nome,
                         email: email,
                         telefone: telefone,
-                    }
+                        cpf: cpf
+                    },
+                    role: parseInt(role)
                 })
             });
             const data = await response.json();
             if (response.ok) {
                 Alert.alert('Cadastro realizado com sucesso!');
             } else {
-                Alert.alert('Erro ao fazer cadastro aqui1', data.message);
+                Alert.alert('Erro ao fazer cadastro', data.message);
             }
         } catch (error) {
-            console.error('Erro ao fazer cadastro: aqui3', error);
-            Alert.alert('Erro ao fazer cadastro aqui2');
+            console.error('Erro ao fazer cadastro:', error);
+            Alert.alert('Erro ao fazer cadastro');
         }
     };
 
     return (
         <View className="bg-white h-full w-full">
             <StatusBar style="light" />
-            
 
-            {/* lampada */}
-            
-
-            {/* titulo e form */}
+            {/* Título e formulário */}
             <View className="h-full w-full flex justify-around pb-1 pt-60">
-                {/* titulo */}
+                {/* Título */}
                 <View className="flex items-center">
                     <Animated.Text entering={FadeInUp.duration(1000).springify()} className="text-white font-bold tracking-wider text-5xl">
                         Cadastro Adm
                     </Animated.Text>
                 </View>
 
-                {/* form */}
+                {/* Formulário */}
                 <View className="flex items-center mx-4 space-y-4 mt-40 mb-5">
                     {/* Nome */}
                     <Animated.View entering={FadeInDown.duration(1000).springify()} className="bg-black/5 p-2 rounded-2xl w-full">
@@ -92,6 +87,10 @@ export default function CadastroAdm() {
                             keyboardType='numeric' 
                         />
                     </Animated.View>
+                    {/* CPF */}
+                    <Animated.View entering={FadeInDown.duration(1000).springify()} className="bg-black/5 p-2 rounded-2xl w-full">
+                        <TextInput placeholder='CPF' placeholderTextColor={'gray'} onChangeText={setCpf} />
+                    </Animated.View>
                     {/* Usuário */}
                     <Animated.View entering={FadeInDown.duration(1000).springify()} className="bg-black/5 p-2 rounded-2xl w-full">
                         <TextInput placeholder='Usuário' placeholderTextColor={'gray'} onChangeText={setUsuario} />
@@ -100,19 +99,31 @@ export default function CadastroAdm() {
                     <Animated.View entering={FadeInDown.delay(200).duration(1000).springify()} className="bg-black/5 p-2 rounded-2xl w-full mb-0">
                         <TextInput placeholder='Senha' placeholderTextColor={'gray'} secureTextEntry onChangeText={setSenha} />
                     </Animated.View>
+                    {/* Role */}
+                    <Animated.View entering={FadeInDown.delay(200).duration(1000).springify()} className="bg-black/5 p-2 rounded-2xl w-full mb-0">
+                        <Picker
+                            selectedValue={role}
+                            onValueChange={(itemValue) => setRole(itemValue)}
+                            style={{ color: 'gray' }}
+                        >
+                            <Picker.Item label="Selecione o role" value="" />
+                            <Picker.Item label="Adm" value="1" />
+                            <Picker.Item label="Server" value="2" />
+                            <Picker.Item label="Aluno" value="3" />
+                        </Picker>
+                    </Animated.View>
                     {/* Botão */}
                     <View className="w-full ">
                         <Animated.View entering={FadeInDown.delay(400).duration(1000).springify()} className="w-full">
                             <TouchableOpacity
                                 onPress={handleSignupAdm}
-                                className="w-full bg-sky-400 p-1 rounded-2xl mb-2">
-                                <Text className="text-xl font-bold text-white text-center">SingUp</Text>
+                                className="w-full bg-sky-400 p-1 rounded-2xl mb-20">
+                                <Text className="text-xl font-bold text-white text-center">Sign Up</Text>
                             </TouchableOpacity>
                         </Animated.View>
                     </View>
                 </View>
             </View>
-
         </View>
-    )
+    );
 }
